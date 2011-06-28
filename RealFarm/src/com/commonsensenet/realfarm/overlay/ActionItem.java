aa64f7333b33e55ccbf95d078318751788d2155d
bolanos <bolanos@d074eaf4-7127-4cba-bfdc-4226f436b952>
2011-06-28 10:14:28 +0000

    Modified to support ids.

--- RealFarm/src/com/commonsensenet/realfarm/overlay/ActionItem.java
+++ RealFarm/src/com/commonsensenet/realfarm/overlay/ActionItem.java
@@ -7,77 +7,96 @@ import android.view.View.OnClickListener;
  * Action item, displayed as menu with icon and text.
  * 
  * @author Lorensius. W. L. T
- *
+ * 
  */
 public class ActionItem {
-	private Drawable icon;
-	private String title;
-	private OnClickListener listener;
-	
+	/** Icon representing the ActionItem */
+	private Drawable mIcon;
+	/** Reference of an object that the ActionItem handles. */
+	private int mId;
+	/** Listener used to handle click events. */
+	private OnClickListener mListener;
+	/** Displayed title of the ActionItem. */
+	private String mTitle;
+
 	/**
-	 * Constructor
+	 * Creates a new ActionItem instance.
 	 */
-	public ActionItem() {}
-	
+	public ActionItem() {
+	}
+
 	/**
 	 * Constructor
 	 * 
-	 * @param icon {@link Drawable} action icon
+	 * @param icon
+	 *            {@link Drawable} action icon
 	 */
 	public ActionItem(Drawable icon) {
-		this.icon = icon;
+		mIcon = icon;
 	}
-	
-	/**
-	 * Set action title
-	 * 
-	 * @param title action title
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	/**
-	 * Get action title
-	 * 
-	 * @return action title
-	 */
-	public String getTitle() {
-		return this.title;
-	}
-	
-	/**
-	 * Set action icon
-	 * 
-	 * @param icon {@link Drawable} action icon
-	 */
-	public void setIcon(Drawable icon) {
-		this.icon = icon;
-	}
-	
+
 	/**
 	 * Get action icon
-	 * @return  {@link Drawable} action icon
+	 * 
+	 * @return {@link Drawable} action icon
 	 */
 	public Drawable getIcon() {
-		return this.icon;
+		return mIcon;
 	}
-	
-	/**
-	 * Set on click listener
-	 * 
-	 * @param listener on click listener {@link View.OnClickListener}
-	 */
-	public void setOnClickListener(OnClickListener listener) {
-		this.listener = listener;
+
+	public int getId() {
+		return mId;
 	}
-	
+
 	/**
 	 * Get on click listener
 	 * 
 	 * @return on click listener {@link View.OnClickListener}
 	 */
 	public OnClickListener getListener() {
-		return this.listener;
+		return mListener;
+	}
+
+	/**
+	 * Get action title
+	 * 
+	 * @return action title
+	 */
+	public String getTitle() {
+		return mTitle;
+	}
+
+	/**
+	 * Set action icon
+	 * 
+	 * @param icon
+	 *            {@link Drawable} action icon
+	 */
+	public void setIcon(Drawable icon) {
+		mIcon = icon;
+	}
+
+	public void setId(int id) {
+		mId = id;
+	}
+
+	/**
+	 * Set on click listener
+	 * 
+	 * @param listener
+	 *            on click listener {@link View.OnClickListener}
+	 */
+	public void setOnClickListener(OnClickListener listener) {
+		mListener = listener;
+	}
+
+	/**
+	 * Set action title
+	 * 
+	 * @param title
+	 *            action title
+	 */
+	public void setTitle(String title) {
+		mTitle = title;
 	}
 }
