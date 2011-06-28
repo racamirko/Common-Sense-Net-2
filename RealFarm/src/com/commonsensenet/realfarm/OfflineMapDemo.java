358b03ccb644a083d80376f792b3d06e9c8e34c9
bolanos <bolanos@d074eaf4-7127-4cba-bfdc-4226f436b952>
2011-06-28 12:43:34 +0000

    updated version with dynamic action items

--- RealFarm/src/com/commonsensenet/realfarm/OfflineMapDemo.java
+++ RealFarm/src/com/commonsensenet/realfarm/OfflineMapDemo.java
@@ -9,7 +9,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
