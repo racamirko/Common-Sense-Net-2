4be1d25ca79e7256aa8fbd41586a2d86f0db35a8
bolanos <bolanos@d074eaf4-7127-4cba-bfdc-4226f436b952>
2011-06-28 12:43:52 +0000

    modified method signature

--- RealFarm/src/com/commonsensenet/realfarm/realFarm.java
+++ RealFarm/src/com/commonsensenet/realfarm/realFarm.java
@@ -1,38 +1,38 @@
 package com.commonsensenet.realfarm;
 
-import com.commonsensenet.realfarm.database.ManageDatabase;
-
 import android.app.Application;
 
+import com.commonsensenet.realfarm.database.ManageDatabase;
+
 /**
- * Main class of application. This class is used to share global information across activities such as the database 
+ * Main class of application. This class is used to share global information
+ * across activities such as the database
+ * 
  * @author Julien Freudiger
- *
+ * 
  */
 public class realFarm extends Application {
 
-	  private int id = 0;
-	  private ManageDatabase db;
+	private ManageDatabase mDb;
+	private int mId = 0;
 
-		
-	  public ManageDatabase getDatabase(){
-		    return db;
-		  }
-	  
-	  public ManageDatabase setDatabase(){
-		  // comment out if you want to reuse existing database
-		  getApplicationContext().deleteDatabase("realFarm.db");
-		  
-		  db = new ManageDatabase(getApplicationContext());
-		  return db;
-	  }
-	  
-	  public void setUserId(int userId){
-		  id = userId;
-	  }
-	  
-	  public int getUserId(){
-		  return id;
-	  }
+	public ManageDatabase getDatabase() {
+		return mDb;
 	}
 
+	public int getUserId() {
+		return mId;
+	}
+
+	public ManageDatabase setDatabase() {
+		// comment out if you want to reuse existing database
+		getApplicationContext().deleteDatabase("realFarm.db");
+
+		mDb = new ManageDatabase(getApplicationContext());
+		return mDb;
+	}
+
+	public void setUserId(int userId) {
+		mId = userId;
+	}
+}
