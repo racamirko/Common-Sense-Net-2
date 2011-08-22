2944019bf435738911391cd1e812c16ad457c0b0
bolanos <bolanos@d074eaf4-7127-4cba-bfdc-4226f436b952>
2011-08-22 09:30:16 +0000

    Removed unused import

--- RealFarm/src/com/commonsensenet/realfarm/overlay/PlotInformationWindow.java
+++ RealFarm/src/com/commonsensenet/realfarm/overlay/PlotInformationWindow.java
@@ -3,7 +3,6 @@ package com.commonsensenet.realfarm.overlay;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
