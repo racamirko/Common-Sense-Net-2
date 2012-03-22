810ab7a2583263f000dbec5f0b78e40679660371
raca <raca@d074eaf4-7127-4cba-bfdc-4226f436b952>
2012-03-22 09:21:31 +0000

    homescreen

--- RealFarm/src/com/commonsensenet/realfarm/homescreen/Homescreen.java
+++ RealFarm/src/com/commonsensenet/realfarm/homescreen/Homescreen.java
@@ -1,7 +1,19 @@
 package com.commonsensenet.realfarm.homescreen;
 
+import com.commonsensenet.realfarm.R;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class Homescreen extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Log.i("Realfarm - homescreen", "App started");
+        setContentView(R.layout.homescreen);
+    }
 
 }
