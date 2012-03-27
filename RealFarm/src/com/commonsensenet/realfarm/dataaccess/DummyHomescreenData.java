dd78fb61c7d53a292cc9f7a6dc3701f17751848a
raca <raca@d074eaf4-7127-4cba-bfdc-4226f436b952>
2012-03-27 22:22:27 +0000

    aggregate view basic sketch

---
+++ RealFarm/src/com/commonsensenet/realfarm/dataaccess/DummyHomescreenData.java
@@ -0,0 +1,89 @@
+package com.commonsensenet.realfarm.dataaccess;
+
+import java.util.Random;
+import java.util.Vector;
+
+import com.commonsensenet.realfarm.R;
+import com.commonsensenet.realfarm.model.Recommendation;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class DummyHomescreenData extends BaseAdapter {
+	protected Context mCtx;
+	protected Activity mActivity;
+	protected LayoutInflater mInflater;
+	protected RealFarmDatabase mDb;
+	protected RealFarmProvider mDataProvider;
+	protected Vector<Recommendation> mInfoPile;
+	
+	public DummyHomescreenData(Context ctx, Activity activity, int numOfItems) {
+		mCtx = ctx;
+		mActivity = activity;
+		mInflater = (LayoutInflater) mCtx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mDb = new RealFarmDatabase(mCtx);
+		mDataProvider = new RealFarmProvider(mDb);
+		generateDummyItems(numOfItems);
+	}
+	
+	private void generateDummyItems(int numOfItems) {
+		Random rn = new Random();
+		// get maximum numbers for each type
+		int maxAct = mDataProvider.getActionNamesList().size()-3;
+		int maxSeed = mDataProvider.getSeedsList().size()-3;
+		// generation
+		mInfoPile = new Vector<Recommendation>(numOfItems);
+		Recommendation tmpObj;
+		for( int runner = 0; runner < numOfItems; ++runner){
+			int actionId = 3+rn.nextInt(maxAct);
+			int seedId = 3+rn.nextInt(maxSeed);
+			tmpObj = new Recommendation(runner, seedId, actionId, "date");
+			mInfoPile.add(tmpObj);
+		}
+	}
+
+	@Override
+	public int getCount() {
+		return mInfoPile.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return mInfoPile.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return mInfoPile.get(position).getId();
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View element;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+        	element = mInflater.inflate(R.layout.aggregate_item, parent, false );
+//        	element.setPadding(2, 2, 2, 2);
+        } else {
+        	element = convertView;
+        }
+
+		// change the title
+        TextView lblBigInfo = (TextView) element.findViewById(R.id.lbl_big_info);
+        TextView lblDesc = (TextView) element.findViewById(R.id.lbl_desc);
+        TextView lblDetail = (TextView) element.findViewById(R.id.lbl_detail);
+
+        Recommendation tmpRec = mInfoPile.get(position);
+        lblBigInfo.setText(String.valueOf(tmpRec.getId()));
+        lblDesc.setText( mDataProvider.getActionNameById(tmpRec.getAction()).getName());
+        lblDetail.setText( mDataProvider.getSeedById(tmpRec.getSeed()).getName());
+        
+        return element;
+	}
+}
--- RealFarm/src/com/commonsensenet/realfarm/homescreen/AggregateView.java
+++ RealFarm/src/com/commonsensenet/realfarm/homescreen/AggregateView.java
@@ -1,17 +1,23 @@
 package com.commonsensenet.realfarm.homescreen;
 
 import com.commonsensenet.realfarm.R;
+import com.commonsensenet.realfarm.dataaccess.DummyHomescreenData;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class AggregateView extends Activity {
 	private String logTag = "AggregateView";
+	protected DummyHomescreenData mDataAdpt;
 	
 	private OnClickListener mCloseListener = new OnClickListener() {
 	    public void onClick(View v) {
@@ -27,6 +33,8 @@ public class AggregateView extends Activity {
         Log.i(logTag, "Activity started");
         setContentView(R.layout.aggregate_view);
         ((ImageButton)findViewById(R.id.btn_aggregate_back)).setOnClickListener(mCloseListener);
+        
+        mDataAdpt = new DummyHomescreenData(getApplicationContext(), this, 20);
 
         String type = getIntent().getStringExtra("type");
         if( type.compareTo("actions") == 0 ){
@@ -45,6 +53,14 @@ public class AggregateView extends Activity {
         	// set actios dataset
         	Toast.makeText(getApplicationContext(), "Yield!", Toast.LENGTH_SHORT).show();
         }
+
+        // unique population for now
+    	ListView listview = (ListView) findViewById(R.id.aggr_items);
+    	listview.setAdapter(mDataAdpt);
+    	listview.setOnItemClickListener(new OnItemClickListener() {
+		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		            Toast.makeText(AggregateView.this, "" + position, Toast.LENGTH_SHORT).show();
+		        }});
         
     }
 
