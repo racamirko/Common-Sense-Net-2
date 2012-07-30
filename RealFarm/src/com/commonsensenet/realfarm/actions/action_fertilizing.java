52c2f4d70adae5eb9ef5c57e9f01b27d24533921
Nguyen Lisa <nguyenlisa@ldm1pc28.epfl.ch>
2012-07-30 16:32:45 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "Modified DB to include the Resources table""""""
    This reverts commit 2c146e8d0b8fa29367ffcbac7b3ae20f315df202.

--- RealFarm/src/com/commonsensenet/realfarm/actions/action_fertilizing.java
+++ RealFarm/src/com/commonsensenet/realfarm/actions/action_fertilizing.java
@@ -16,15 +16,14 @@ import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 
 import com.commonsensenet.realfarm.Global;
 import com.commonsensenet.realfarm.HelpEnabledActivityOld;
@@ -76,7 +75,7 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 		setHelpIcon(findViewById(R.id.helpIndicator));
 
 		System.out.println("plant done");
-		
+
 		resultsMap = new HashMap<String, String>();
 		resultsMap.put("units_fert", "0");
 		resultsMap.put("months_fert", "0");
@@ -126,9 +125,12 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 			public void onClick(View v) {
 				stopAudio();
 				Log.d("in variety fert dialog", "in dialog");
-				
-				ArrayList<DialogData> m_entries = mDataProvider.getFertilizers();
-				displayDialog(v, m_entries, "fert_var_sel", "Choose the fertilizer", R.raw.problems, R.id.dlg_lbl_var_fert, R.id.var_fert_tr, 0);
+
+				ArrayList<DialogData> m_entries = mDataProvider
+						.getFertilizers();
+				displayDialog(v, m_entries, "fert_var_sel",
+						"Choose the fertilizer", R.raw.problems,
+						R.id.dlg_lbl_var_fert, R.id.var_fert_tr, 0);
 			}
 		});
 
@@ -136,9 +138,12 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 			public void onClick(View v) {
 				stopAudio();
 				Log.d("in units fert dialog", "in dialog");
-				
-				ArrayList<DialogData> m_entries = mDataProvider.getUnits(RealFarmDatabase.ACTION_NAME_FERTILIZE_ID);
-				displayDialog(v, m_entries, "units_fert", "Choose the unit", R.raw.problems, R.id.dlg_lbl_units_fert, R.id.units_fert_tr, 1);
+
+				ArrayList<DialogData> m_entries = mDataProvider
+						.getUnits(RealFarmDatabase.ACTION_NAME_FERTILIZE_ID);
+				displayDialog(v, m_entries, "units_fert", "Choose the unit",
+						R.raw.problems, R.id.dlg_lbl_units_fert,
+						R.id.units_fert_tr, 1);
 			}
 		});
 
@@ -146,8 +151,13 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 			public void onClick(View v) {
 				stopAudio();
 				Log.d("in variety sowing dialog", "in dialog");
-				
-				displayDialogNP("Choose the day", "day_fert_int", R.raw.dateinfo, 1, 31, Calendar.getInstance().get(Calendar.DAY_OF_MONTH), 1, 0, R.id.dlg_lbl_day_fert, R.id.day_fert_tr, R.raw.dateinfo, R.raw.dateinfo, R.raw.dateinfo, R.raw.dateinfo);
+
+				displayDialogNP("Choose the day", "day_fert_int",
+						R.raw.dateinfo, 1, 31,
+						Calendar.getInstance().get(Calendar.DAY_OF_MONTH), 1,
+						0, R.id.dlg_lbl_day_fert, R.id.day_fert_tr,
+						R.raw.dateinfo, R.raw.dateinfo, R.raw.dateinfo,
+						R.raw.dateinfo);
 
 			}
 		});
@@ -156,8 +166,11 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 			public void onClick(View v) {
 				stopAudio();
 				Log.d("in variety sowing dialog", "in dialog");
-				
-				displayDialogNP("Choose the amount", "fert_no", R.raw.dateinfo, 0, 100, 1, 0.25, 2, R.id.dlg_lbl_unit_no_fert, R.id.units_fert_tr, R.raw.dateinfo, R.raw.dateinfo, R.raw.dateinfo, R.raw.dateinfo);
+
+				displayDialogNP("Choose the amount", "fert_no", R.raw.dateinfo,
+						0, 100, 1, 0.25, 2, R.id.dlg_lbl_unit_no_fert,
+						R.id.units_fert_tr, R.raw.dateinfo, R.raw.dateinfo,
+						R.raw.dateinfo, R.raw.dateinfo);
 
 			}
 		});
@@ -166,9 +179,18 @@ public class action_fertilizing extends HelpEnabledActivityOld implements
 			public void onClick(View v) {
 				stopAudio();
 				Log.d("in variety sowing dialog", "in dialog");
+<<<<<<< HEAD
 				
 				ArrayList<DialogData> m_entries = mDataProvider.getDialogData(RealFarmDatabase.DIALOG_MONTH_ID);
 				displayDialog(v, m_entries, "months_fert", "Select the month", R.raw.bagof50kg, R.id.dlg_lbl_month_fert, R.id.day_fert_tr, 0);
+=======
+
+				ArrayList<DialogData> m_entries = DialogArrayLists
+						.getMonthArray(v);
+				displayDialog(v, m_entries, "months_fert", "Select the month",
+						R.raw.bagof50kg, R.id.dlg_lbl_month_fert,
+						R.id.day_fert_tr, 0);
+>>>>>>> Modified DB to include the Resources table
 			}
 
 		});
@@ -272,12 +294,12 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 				// tracks the application usage.
 				ApplicationTracker.getInstance().logEvent(EventType.CLICK,
 						LOG_TAG, "ok_btn");
-				
+
 				units_fert = resultsMap.get("units_fert");
 				months_fert = resultsMap.get("months_fert");
 				fert_var_sel = resultsMap.get("fert_var_sel");
 				day_fert_int = Integer.parseInt(resultsMap.get("day_fert_int"));
-				fert_no = (int)(Double.parseDouble(resultsMap.get("fert_no")));
+				fert_no = (int) (Double.parseDouble(resultsMap.get("fert_no")));
 
 				int flag1, flag2, flag3;
 				if (units_fert.toString().equalsIgnoreCase("0") || fert_no == 0) {
@@ -294,10 +316,11 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 				} else {
 					flag1 = 0;
 					View tr_feedback = (View) findViewById(R.id.units_fert_tr);
-					tr_feedback.setBackgroundResource(android.R.drawable.list_selector_background);
+					tr_feedback
+							.setBackgroundResource(android.R.drawable.list_selector_background);
 				}
 
-				if (fert_var_sel.toString().equalsIgnoreCase("0")) { 
+				if (fert_var_sel.toString().equalsIgnoreCase("0")) {
 
 					flag2 = 1;
 
@@ -313,7 +336,8 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 
 					flag2 = 0;
 					View tr_feedback = (View) findViewById(R.id.var_fert_tr);
-					tr_feedback.setBackgroundResource(android.R.drawable.list_selector_background);
+					tr_feedback
+							.setBackgroundResource(android.R.drawable.list_selector_background);
 				}
 
 				if (months_fert.toString().equalsIgnoreCase("0")
@@ -334,17 +358,18 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 					flag3 = 0;
 					day_fert_sel = day_fert_sel_1 + "." + months_fert;
 					View tr_feedback = (View) findViewById(R.id.var_fert_tr);
-					tr_feedback.setBackgroundResource(android.R.drawable.list_selector_background);
+					tr_feedback
+							.setBackgroundResource(android.R.drawable.list_selector_background);
 				}
 
 				if (flag1 == 0 && flag2 == 0 && flag3 == 0) {
 
 					System.out.println("fertilizing writing");
-					long result = mDataProvider.setFertilizing(Global.plotId, fert_no,
+					mDataProvider.setFertilizing(Global.plotId, fert_no,
 							fert_var_sel, units_fert, day_fert_sel, 1, 0);
-					
-					//System.out.println("fertilizing reading");
-					//mDataProvider.getfertizing();
+
+					// System.out.println("fertilizing reading");
+					// mDataProvider.getfertizing();
 
 					// tracks the application usage.
 					ApplicationTracker.getInstance().logEvent(EventType.CLICK,
@@ -380,8 +405,9 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 	public boolean onLongClick(View v) {
 
 		if (v.getId() == R.id.dlg_lbl_var_fert) {
-			
-			Toast.makeText(action_fertilizing.this, ((TextView)(v)).getText(), Toast.LENGTH_LONG).show();
+
+			Toast.makeText(action_fertilizing.this, ((TextView) (v)).getText(),
+					Toast.LENGTH_LONG).show();
 
 			playAudioalways(R.raw.selecttypeoffertilizer);
 			ShowHelpIcon(v);
@@ -391,7 +417,7 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 					LOG_TAG, "type_fertilizer");
 
 		}
-		
+
 		if (v.getId() == R.id.dlg_lbl_units_fert) {
 
 			playAudioalways(R.raw.selecttheunits);
@@ -484,132 +510,166 @@ System.out.println("*********************GETTING PESTICIDE ID*******************
 
 	}
 
-	private void putBackgrounds(DialogData choice, TextView var_text, int imageType){
-		if(choice.getBackgroundRes() != -1) var_text.setBackgroundResource(choice.getBackgroundRes());
-		if(imageType == 1 || imageType == 2){
-			BitmapDrawable bd=(BitmapDrawable) mParentReference.getResources().getDrawable(choice.getImageRes());
+	private void putBackgrounds(DialogData choice, TextView var_text,
+			int imageType) {
+		if (choice.getBackgroundRes() != -1)
+			var_text.setBackgroundResource(choice.getBackgroundRes());
+		if (imageType == 1 || imageType == 2) {
+			BitmapDrawable bd = (BitmapDrawable) mParentReference
+					.getResources().getDrawable(choice.getImageRes());
 			int width = bd.getBitmap().getWidth();
-			if(width>80) width = 80;
-			
-			LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		    llp.setMargins(10, 0, 80-width-20, 0); 
-		    var_text.setLayoutParams(llp);
-			
+			if (width > 80)
+				width = 80;
+
+			LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			llp.setMargins(10, 0, 80 - width - 20, 0);
+			var_text.setLayoutParams(llp);
+
 			var_text.setBackgroundResource(choice.getImageRes());
-			if (imageType == 1) var_text.setTextColor(Color.TRANSPARENT);
-			else{ 
-			    var_text.setGravity(Gravity.TOP); 
-			    var_text.setPadding(0, 0, 0, 0); 
-			    var_text.setTextSize(20); 
+			if (imageType == 1)
+				var_text.setTextColor(Color.TRANSPARENT);
+			else {
+				var_text.setGravity(Gravity.TOP);
+				var_text.setPadding(0, 0, 0, 0);
+				var_text.setTextSize(20);
 				var_text.setTextColor(Color.BLACK);
 			}
 		}
 	}
-	
-	private void displayDialog(View v, final ArrayList<DialogData> m_entries, final String mapEntry, final String title, int entryAudio, final int varText, final int trFeedback, final int imageType){ 
+
+	private void displayDialog(View v, final ArrayList<DialogData> m_entries,
+			final String mapEntry, final String title, int entryAudio,
+			final int varText, final int trFeedback, final int imageType) {
 		final Dialog dialog = new Dialog(v.getContext());
 		dialog.setContentView(R.layout.mc_dialog);
 		dialog.setTitle(title);
 		dialog.setCancelable(true);
 		dialog.setCanceledOnTouchOutside(true);
 
-		DialogAdapter m_adapter = new DialogAdapter(v.getContext(), R.layout.mc_dialog_row, m_entries);
-		ListView mList = (ListView)dialog.findViewById(R.id.liste);
+		DialogAdapter m_adapter = new DialogAdapter(v.getContext(),
+				R.layout.mc_dialog_row, m_entries);
+		ListView mList = (ListView) dialog.findViewById(R.id.liste);
 		mList.setAdapter(m_adapter);
 
 		dialog.show();
 		playAudio(entryAudio); // TODO: onOpen
 
-		mList.setOnItemClickListener(new OnItemClickListener(){ // TODO: adapt the audio in the db
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		mList.setOnItemClickListener(new OnItemClickListener() { // TODO: adapt
+			// the audio
+			// in the db
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
 				// Does whatever is specific to the application
-				Log.d("var "+position+" picked ", "in dialog");
+				Log.d("var " + position + " picked ", "in dialog");
 				TextView var_text = (TextView) findViewById(varText);
 				DialogData choice = m_entries.get(position);
 				var_text.setText(choice.getShortName());
-				resultsMap.put(mapEntry, choice.getValue());  
+				resultsMap.put(mapEntry, choice.getValue());
 				View tr_feedback = (View) findViewById(trFeedback);
-				tr_feedback.setBackgroundResource(android.R.drawable.list_selector_background);
-				
-				// put backgrounds (specific to the application) TODO: optimize the resize
+				tr_feedback
+						.setBackgroundResource(android.R.drawable.list_selector_background);
+
+				// put backgrounds (specific to the application) TODO: optimize
+				// the resize
 				putBackgrounds(choice, var_text, imageType);
-				
+
 				// tracks the application usage.
-				ApplicationTracker.getInstance().logEvent(
-						EventType.CLICK, LOG_TAG, title,
-						choice.getValue());
-				
-				Toast.makeText(mParentReference, resultsMap.get(mapEntry), Toast.LENGTH_SHORT).show();
-						
+				ApplicationTracker.getInstance().logEvent(EventType.CLICK,
+						LOG_TAG, title, choice.getValue());
+
+				Toast.makeText(mParentReference, resultsMap.get(mapEntry),
+						Toast.LENGTH_SHORT).show();
+
 				// onClose
 				dialog.cancel();
 				int iden = choice.getAudioRes();
-				//view.getContext().getResources().getIdentifier("com.commonsensenet.realfarm:raw/" + choice.getAudio(), null, null);
+				// view.getContext().getResources().getIdentifier("com.commonsensenet.realfarm:raw/"
+				// + choice.getAudio(), null, null);
 				playAudio(iden);
-			}});
+			}
+		});
 
-		mList.setOnItemLongClickListener(new OnItemLongClickListener(){
+		mList.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) { // TODO: adapt the audio in the db
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) { // TODO: adapt the audio in the db
 				int iden = m_entries.get(position).getAudioRes();
-				//view.getContext().getResources().getIdentifier("com.commonsensenet.realfarm:raw/" + m_entries.get(position).getAudio(), null, null);
+				// view.getContext().getResources().getIdentifier("com.commonsensenet.realfarm:raw/"
+				// + m_entries.get(position).getAudio(), null, null);
 				playAudioalways(iden);
 				return true;
-			}});
+			}
+		});
 	}
-	
-	private void displayDialogNP(String title, final String mapEntry, int openAudio, double min, double max, double init, double inc, int nbDigits, int textField, int tableRow, final int okAudio, final int cancelAudio, final int infoOkAudio, final int infoCancelAudio){ 
+
+	private void displayDialogNP(String title, final String mapEntry,
+			int openAudio, double min, double max, double init, double inc,
+			int nbDigits, int textField, int tableRow, final int okAudio,
+			final int cancelAudio, final int infoOkAudio,
+			final int infoCancelAudio) {
 
 		final Dialog dialog = new Dialog(mParentReference);
 		dialog.setTitle(title);
 		dialog.setCancelable(true);
 		dialog.setCanceledOnTouchOutside(true);
 		playAudio(openAudio); // opening audio
-		
-		if(!resultsMap.get(mapEntry).equals("0") && !resultsMap.get(mapEntry).equals("-1")) init = Double.valueOf(resultsMap.get(mapEntry));
-		
-		NumberPicker np = new NumberPicker(mParentReference, min, max, init, inc, nbDigits);
+
+		if (!resultsMap.get(mapEntry).equals("0")
+				&& !resultsMap.get(mapEntry).equals("-1"))
+			init = Double.valueOf(resultsMap.get(mapEntry));
+
+		NumberPicker np = new NumberPicker(mParentReference, min, max, init,
+				inc, nbDigits);
 		dialog.setContentView(np);
-		
+
 		final TextView tw_sow = (TextView) findViewById(textField);
 		final View tr_feedback = (View) findViewById(tableRow);
 
-		final TextView tw = (TextView)dialog.findViewById(R.id.tw);
-		ImageButton ok = (ImageButton)dialog.findViewById(R.id.ok);
-		ImageButton cancel = (ImageButton)dialog.findViewById(R.id.cancel);
-        ok.setOnClickListener(new View.OnClickListener(){ 
+		final TextView tw = (TextView) dialog.findViewById(R.id.tw);
+		ImageButton ok = (ImageButton) dialog.findViewById(R.id.ok);
+		ImageButton cancel = (ImageButton) dialog.findViewById(R.id.cancel);
+		ok.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				String result = tw.getText().toString(); 
-				resultsMap.put(mapEntry, result); 
+				String result = tw.getText().toString();
+				resultsMap.put(mapEntry, result);
 				tw_sow.setText(result);
-				tr_feedback.setBackgroundResource(android.R.drawable.list_selector_background);
-				Toast.makeText(mParentReference , result, Toast.LENGTH_LONG).show();
+				tr_feedback
+						.setBackgroundResource(android.R.drawable.list_selector_background);
+				Toast.makeText(mParentReference, result, Toast.LENGTH_LONG)
+						.show();
 				dialog.cancel();
 				playAudio(okAudio); // ok audio
-		}});
-        cancel.setOnClickListener(new View.OnClickListener(){ 
+			}
+		});
+		cancel.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				dialog.cancel();
 				playAudio(cancelAudio); // cancel audio
-				ApplicationTracker.getInstance().logEvent(EventType.CLICK, LOG_TAG, "amount", "cancel");
-		}});
-        ok.setOnLongClickListener(new View.OnLongClickListener(){ 
+				ApplicationTracker.getInstance().logEvent(EventType.CLICK,
+						LOG_TAG, "amount", "cancel");
+			}
+		});
+		ok.setOnLongClickListener(new View.OnLongClickListener() {
 			public boolean onLongClick(View view) {
 				playAudio(infoOkAudio); // info audio
 				return true;
-		}});
-        cancel.setOnLongClickListener(new View.OnLongClickListener(){ 
+			}
+		});
+		cancel.setOnLongClickListener(new View.OnLongClickListener() {
 			public boolean onLongClick(View view) {
 				playAudio(infoCancelAudio); // info audio
 				return true;
-		}});
-        tw.setOnLongClickListener(new View.OnLongClickListener(){ 
+			}
+		});
+		tw.setOnLongClickListener(new View.OnLongClickListener() {
 			public boolean onLongClick(View view) {
-				String num = tw.getText().toString();
+				// String num = tw.getText().toString();
 				playAudio(R.raw.dateinfo); // info audio
 				return false;
-		}});
-        				
+			}
+		});
+
 		dialog.show();
 	}
 
